3bd19e00a4efeb43428cc36a822448d4d2329505
RishanJR <rishanrajkumar@yahoo.com>
2020-09-25 13:48:22 +0400

    Getting 3 numbers from the user

--- arithmetic_computation.sh
+++ arithmetic_computation.sh
@@ -2,3 +2,13 @@
 
 echo -e "\nWelcome to Arithmetic Computation and sorting"
 echo -e "This program computes different arithmetic expressions and sorts the results\n"
+
+#Input three numbers
+echo "Enter three numbers (giving enter after each number)"
+read num1
+read num2
+read num3
+
+echo -e "\nThe numbers entered are:"
+echo $num1 $num2 $num3
+
